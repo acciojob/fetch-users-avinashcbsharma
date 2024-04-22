d18e39ee5c84f12c2ce109857a54e8a15da297c8
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-04-22 11:50:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,35 +2,37 @@ import React, { useState } from 'react'
 import axios from 'axios'
 
 export default function App () {
-  const [isvissible, setIsvissible] = useState(true)
-  const [err, setErr] = useState('')
+  const [isloading, setIsloading] = useState(false);
   const [users, setUsers] = useState([])
   
     let fetchoperation = async () => {
+        setIsloading(true);
       await axios({
         url: 'https://reqres.in/api/users',
         module: 'Get'
       })
         .then(response => {
-          setIsvissible(false)
+          setIsloading(false)
           setUsers(response.data.data);
         })
         .catch(function (error) {
           let err = JSON.stringify(error.message)
-          console.log(err)
-          setErr(err)
+          console.log(err)          
         })
-      }
+    }
+    
+      
 
   return (
     <div id='container'>
       <div className='heading flx'>
-        <span>Blue Whales</span>
-        <button className='btn' onClick={fetchoperation}>
-          Get User List
-        </button>
+            <span>Blue Whales</span>
+            <button className='btn' onClick={fetchoperation}>
+            Get User List
+            </button>
       </div>
-      <table id="table1" >      
+     
+    <table className="table1" >      
         <thead>
           <tr>                        
             <th>First Name</th>
@@ -39,8 +41,12 @@ export default function App () {
             <th>Avatar</th>
           </tr>
         </thead>
-      
-        <tbody>
+    </table>     
+    {isloading ? 
+    (<p className='flx loding'>No data found to display</p> ) : users.length > 0 ? 
+    ( 
+    <table className="table1" >
+    <tbody>
         {
           users.map(function(item){
             return (
@@ -54,8 +60,9 @@ export default function App () {
           })
         }
         </tbody>
-      </table>      
-      {(err || isvissible) && <p className='flx loding'>No data found to display</p>}      
+    </table> 
+    ) : (<p className='flx loding'>No data found to display</p>)
+    }    
     </div>
   )
 }
